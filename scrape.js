f69c8e39cafbcb277d5c40d9dd7b58bf68a1e106
Brady Holt <brady.holt@gmail.com>
2015-10-30 13:16:41 -0500

    Take out hardcoded class info; allow for class discovery

--- scrape.js
+++ scrape.js
@@ -1,9 +1,11 @@
-var casper = require('casper').create();
+var casper = require('casper').create({
+    clientScripts: ['casper-scripts/jquery-1.11.3.min.js']
+});
 var util = require('utils');
 var fs = require('fs');
 var config = require('config');
 
-casper.start('http://lbs.hfbc.org/default.aspx?page=3062', function() {
+casper.start(config.arena_url + '/default.aspx?page=3062', function() {
     this.fill('form', {
 	'ctl08$ctl01$txtLoginId': config.arena_username,
         'ctl08$ctl01$txtPassword': config.arena_password
@@ -14,68 +16,101 @@ casper.thenClick('#ctl08_ctl01_btnSignin').waitForText("Welcome!", function(){
 	this.echo("Login successful.");
 });
 
-casper.thenOpen('http://lbs.hfbc.org/default.aspx?page=3077&group=2177').waitForText("CLICK HERE TO LOG OUT", function(){
-     this.echo("Members page loaded.");
-     var form_info = this.evaluate(function(){
-        var res={};
-        f=document.forms[0];
-        f.onsubmit= function() {
-            //iterate the form fields
-            var post={};
-            for(i=0; i<f.elements.length; i++) {
-               post[f.elements[i].name]=f.elements[i].value;
-            }
+casper.thenOpen(config.arena_url + '/default.aspx?page=3071').waitForText("Select Class Name to View Members", function(){
+    this.echo("Class list page loaded.");
 
-            post["ctl08$ctl11$stGroupMain$ctl22$dgMembers$ctl28$dgMembers_ibExport.x"] = "8";
-            post["ctl08$ctl11$stGroupMain$ctl22$dgMembers$ctl28$dgMembers_ibExport.y"] = "7";
+    var classes = this.evaluate(function(){
+          var classList = [];
+          $('#ctl08_ctl02_dgGroups tr.listItem td:first a').each(function() {
+              var url = $(this).attr("href");
+              var name = $(this).text();
+              var id = url.match(/\S*group\=(\d+)/i)[1];
+              classList.push({ id: id, name: name});
+          });
+          return classList;
+      });
 
-            res.action = f.action;
-            res.post = post;
-            return false; //Stop form submission
-        }
-
-        //Trigger the click on the link.
-        var l = $("#ctl08_ctl11_stGroupMain_ctl22_dgMembers_ctl28_dgMembers_ibExport");
-        l.removeAttr("onclick");
-        l.click();
-
-        return res; //Return the form data to casper
-     });
-
-     this.echo("Downloading roster...");
-     casper.download(form_info.action, "data/roster.html", "POST", form_info.post);
+      casper.then(function(){
+          writeMetaFile(classes);
+          classes.forEach(function(c){
+              downloadRoster(c.id, c.name);
+              downloadAttendance(c.id, c.name);
+          });
+      });
 });
 
-casper.thenOpen('http://lbs.hfbc.org/default.aspx?page=3077&group=2177&tab=AggregateMembers').waitForText("CLICK HERE TO LOG OUT", function(){
-     this.echo("Members page loaded.");
-     var form_info = this.evaluate(function(){
-        var res={};
-        f=document.forms[0];
-        f.onsubmit= function() {
-            //iterate the form fields
-            var post={};
-            for(i=0; i<f.elements.length; i++) {
-               post[f.elements[i].name]=f.elements[i].value;
+function writeMetaFile(classes){
+    fs.write(config.scrape_data_path + "classes.json", JSON.stringify(classes), 'w');
+}
+
+function downloadRoster(classId, className) {
+    casper.thenOpen(config.arena_url + '/default.aspx?page=3077&group=' + classId).waitForText("CLICK HERE TO LOG OUT", function(){
+         this.echo(className + ": Members page loaded.");
+         var form_info = this.evaluate(function(){
+            var res={};
+            f=document.forms[0];
+            f.onsubmit= function() {
+                //iterate the form fields
+                var post={};
+                for(i=0; i<f.elements.length; i++) {
+                   post[f.elements[i].name]=f.elements[i].value;
+                }
+
+                post["ctl08$ctl11$stGroupMain$ctl22$dgMembers$ctl28$dgMembers_ibExport.x"] = "8";
+                post["ctl08$ctl11$stGroupMain$ctl22$dgMembers$ctl28$dgMembers_ibExport.y"] = "7";
+
+                res.action = f.action;
+                res.post = post;
+                return false; //Stop form submission
             }
 
-            post["ctl08$ctl11$stGroupMain$ctl22$dgPersons$ctl28$dgPersons_ibExport.x"] = "4";
-            post["ctl08$ctl11$stGroupMain$ctl22$dgPersons$ctl28$dgPersons_ibExport.y"] = "10";
+            //Trigger the click on the link.
+            var l = $("#ctl08_ctl11_stGroupMain_ctl22_dgMembers_ctl28_dgMembers_ibExport");
+            l.removeAttr("onclick");
+            l.click();
 
-            res.action = f.action;
-            res.post = post;
-            return false; //Stop form submission
-        }
+            return res; //Return the form data to casper
+         });
 
-        //Trigger the click on the link.
-        var l = $("#ctl08_ctl11_stGroupMain_ctl22_dgPersons_ctl28_dgPersons_ibExport");
-        l.removeAttr("onclick");
-        l.click();
+         this.echo(className + ": Downloading roster data...");
+         casper.download(form_info.action, config.scrape_data_path + classId + "_roster.html", "POST", form_info.post);
+         this.echo(className + ": Roster data downloaded.");
+    });
+}
 
-        return res; //Return the form data to casper
-     });
+function downloadAttendance(classId, className) {
+    casper.thenOpen(config.arena_url + '/default.aspx?page=3077&group=' + classId + '&tab=AggregateMembers').waitForText("CLICK HERE TO LOG OUT", function(){
+         this.echo(className + ": Attendance page loaded.");
+         var form_info = this.evaluate(function(){
+            var res={};
+            f=document.forms[0];
+            f.onsubmit= function() {
+                //iterate the form fields
+                var post={};
+                for(i=0; i<f.elements.length; i++) {
+                   post[f.elements[i].name]=f.elements[i].value;
+                }
 
-     this.echo("Downloading attendance...");
-     casper.download(form_info.action, "data/attendance.html", "POST", form_info.post);
-});
+                post["ctl08$ctl11$stGroupMain$ctl22$dgPersons$ctl28$dgPersons_ibExport.x"] = "4";
+                post["ctl08$ctl11$stGroupMain$ctl22$dgPersons$ctl28$dgPersons_ibExport.y"] = "10";
+
+                res.action = f.action;
+                res.post = post;
+                return false; //Stop form submission
+            }
+
+            //Trigger the click on the link.
+            var l = $("#ctl08_ctl11_stGroupMain_ctl22_dgPersons_ctl28_dgPersons_ibExport");
+            l.removeAttr("onclick");
+            l.click();
+
+            return res; //Return the form data to casper
+         });
+
+         this.echo(className + ": Downloading attendance data...");
+         casper.download(form_info.action, config.scrape_data_path + classId + "_attendance.html", "POST", form_info.post);
+         this.echo(className + ": Attendance data downloaded.");
+    });
+}
 
 casper.run();
--- update.js
+++ update.js
@@ -1,79 +1,125 @@
-var Spreadsheet = require('edit-google-spreadsheet');
-var config = require('./config');
-var _ = require('lodash');
-var fs = require('fs');
-var google = require('googleapis');
-var OAuth2Client = google.auth.OAuth2;
-var spreadsheetManager = require('./lib/spreadsheet-manager');
+"use strict";
 
-var rosterData = JSON.parse(fs.readFileSync('data/roster.json', 'utf8'));
-var attendanceData = JSON.parse(fs.readFileSync('data/attendance.json', 'utf8'));
+let Spreadsheet = require('edit-google-spreadsheet');
+let config = require('./config');
+let _ = require('lodash');
+let fs = require('fs');
+let google = require('googleapis');
+let OAuth2Client = google.auth.OAuth2;
+let spreadsheetManager = require('./lib/spreadsheet-manager');
+var tabletojson = require('tabletojson');
 
-var fieldMappings = _.invert(rosterData[0]);
-rosterData.shift();
-
-var rosterFormatted = _.map(rosterData, function(d) {
-    return [ d[fieldMappings["last_name"]], d[fieldMappings["first_name"]], d[fieldMappings["gender"]], d[fieldMappings["person_birthdate"]],
-             d[fieldMappings["person_email"]], d[fieldMappings["home_phone"]], d[fieldMappings["mobile_phone"]],
-             d[fieldMappings["address"]], d[fieldMappings["city"]], d[fieldMappings["state"]], d[fieldMappings["postal_code"]],
-             d[fieldMappings["record_status"]], d[fieldMappings["member_role"]]];
-});
-
-var activeRoster = _.filter(rosterFormatted, function(d) {
-    return d[11] == "Active" && (d[12] == "Member" || d[12].indexOf('Leader') > -1);
-});
-
-var inActiveRoster = _.filter(rosterFormatted, function(d) {
-    return d[11] == "Inactive";
-});
-
-var oauth2Client = new OAuth2Client(config.client_id, config.client_secret, 'http://www.myauthorizedredirecturl.com');
+let oauth2Client = new OAuth2Client(config.client_id, config.client_secret, 'http://www.myauthorizedredirecturl.com');
 
 oauth2Client.setCredentials({
-  access_token: config.access_token,
-  refresh_token: config.refresh_token
+    access_token: config.access_token,
+    refresh_token: config.refresh_token
 });
 
-var manager = new spreadsheetManager(oauth2Client);
-manager.prepSheet('Arena Test 1', config.template_spreadsheet_id)
-.then(function(sheetData) {
-    writeData(sheetData.spreadsheetId, sheetData.worksheets['Roster'], activeRoster);
-    writeData(sheetData.spreadsheetId, sheetData.worksheets['Inactive'], inActiveRoster);
-    writeData(sheetData.spreadsheetId, sheetData.worksheets['Attendance'], getAttendanceData(attendanceData));
-}).catch(function(err){
-    console.log(err);
+oauth2Client.refreshAccessToken(function(err, tokens) {
+    oauth2Client.setCredentials({
+        access_token: tokens.access_token,
+        refresh_token: tokens.refresh_token
+    });
+
+    start();
 });
 
-function writeData(spreadsheetId, worksheetId, data) {
-  Spreadsheet.load({
-    debug: true,
-    spreadsheetId: spreadsheetId,
-    worksheetId: worksheetId,
+function start() {
+    var manager = new spreadsheetManager(oauth2Client);
+    var classes = require('./data/classes.json');
 
-    // OR 3. OAuth2 (See get_oauth2_permissions.js)
-    oauth2: {
-      client_id: config.client_id,
-      client_secret: config.client_secret,
-      refresh_token: config.refresh_token
-    }
-  }, function sheetReady(err, spreadsheet) {
-      spreadsheet.add(data);
+    classes.forEach(function(c){
+        let classData = readData(c.id);
+        manager.prepSheet( {
+            name: c.name,
+            templateId: config.template_spreadsheet_id,
+            worksheetNames: ['Roster', 'Visitors', 'Attendance', 'Email Lists', 'Inactive'],
+            worksheetRows: 500,
+            worksheetColumns: 15
+        }).then(function(sheetData) {
+                var roster = getRosterData(classData.roster.data, function(status, role) {
+                    return status == "Active" && (role == "Member" || role.indexOf('Leader') > -1);
+                });
 
-      spreadsheet.send(function(err) {
-        if(err) throw err;
-      });
-  });
+                var inactive = getRosterData(classData.roster.data, function(status, role) {
+                    return status == "Inactive";
+                });
+
+                var attendance = getAttendanceData(classData.attendance);
+
+                writeData(sheetData.spreadsheetId, sheetData.worksheets['Roster'], roster);
+                writeData(sheetData.spreadsheetId, sheetData.worksheets['Inactive'], inactive);
+                writeData(sheetData.spreadsheetId, sheetData.worksheets['Attendance'], attendance);
+
+            }).catch(function(err) {
+                console.log(err);
+            });
+    });
 }
 
-function getAttendanceData(rawData) {
-  for(var i=9; i<= 20; i++){
-      var dirtyDate = rawData[0][i.toString()];
-      rawData[0][i.toString()] = dirtyDate.substring(0, dirtyDate.length - 2);
-  }
+function readData(classId) {
+    let rosterHtml = fs.readFileSync(config.scrape_data_path + '/' + classId + '_roster.html', 'utf8');
+    let rosterData = tabletojson.convert(rosterHtml)[0];
 
-  var attenanceFormatted = _.map(rawData, function(d) {
-      return [ d["0"], d["20"], d["19"], d["18"], d["17"], d["16"], d["15"], d["14"], d["13"], d["12"], d["11"], d["10"], d["9"] ];
-  });
+    let attendanceHtml = fs.readFileSync(config.scrape_data_path + '/' + classId + '_attendance.html', 'utf8');
+    let attendanceData = tabletojson.convert(attendanceHtml)[0];
 
-  return attenanceFormatted;
- }
+    let fieldMappings = _.invert(rosterData[0]);
+    rosterData.shift();
+
+    let rosterFormatted = _.map(rosterData, function(d) {
+        return [d[fieldMappings["last_name"]], d[fieldMappings["first_name"]], d[fieldMappings["gender"]], d[fieldMappings["person_birthdate"]],
+            d[fieldMappings["person_email"]], d[fieldMappings["home_phone"]], d[fieldMappings["mobile_phone"]],
+            d[fieldMappings["address"]], d[fieldMappings["city"]], d[fieldMappings["state"]], d[fieldMappings["postal_code"]],
+            d[fieldMappings["record_status"]], d[fieldMappings["member_role"]]
+        ];
+    });
+    return {
+        roster: {
+            fields: fieldMappings,
+            data: rosterFormatted
+        },
+        attendance: attendanceData
+    };
+}
+
+function writeData(spreadsheetId, worksheetId, data) {
+    Spreadsheet.load({
+        debug: true,
+        spreadsheetId: spreadsheetId,
+        worksheetId: worksheetId,
+
+        // OR 3. OAuth2 (See get_oauth2_permissions.js)
+        oauth2: {
+            client_id: config.client_id,
+            client_secret: config.client_secret,
+            refresh_token: config.refresh_token
+        }
+    }, function sheetReady(err, spreadsheet) {
+        spreadsheet.add(data);
+
+        spreadsheet.send(function(err) {
+            if (err) throw err;
+        });
+    });
+}
+
+function getRosterData(sourceData, filter) {
+    return _.filter(sourceData, function(d) {
+        return filter(d[11], d[12]);
+    });
+}
+
+function getAttendanceData(sourceData) {
+    for (var i = 9; i <= 20; i++) {
+        var dirtyDate = sourceData[0][i.toString()];
+        sourceData[0][i.toString()] = dirtyDate.substring(0, dirtyDate.length - 2);
+    }
+
+    var attenanceFormatted = _.map(sourceData, function(d) {
+        return [d["0"], d["20"], d["19"], d["18"], d["17"], d["16"], d["15"], d["14"], d["13"], d["12"], d["11"], d["10"], d["9"]];
+    });
+
+    return attenanceFormatted;
+}
