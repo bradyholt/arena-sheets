881fd21951466bd06c187adabb3c185d48351724
Brady Holt <brady.holt@gmail.com>
2015-11-06 17:24:50 -0600

    Fix misc issues related to real class data testing

--- arena-sheets.js
+++ arena-sheets.js
@@ -46,78 +46,110 @@ function updateSheets(oauth2Client) {
     var classes = require('./data/classes.json');
 
     var worksheets = [
-        { name: 'Roster', rows: 500, col: 15 },
-        { name: 'Visitors', rows: 500, col: 15 },
-        { name: 'Attendance', rows: 500, col: 15 },
-        { name: 'Email Lists', rows: 500, col: 15 },
-        { name: 'Inactive', rows: 500, col: 15 }
+        { name: 'Roster', rows: 100, col: 15 },
+        { name: 'Visitors', rows: 100, col: 15 },
+        { name: 'Attendance', rows: 100, col: 15 },
+        { name: 'Email Lists', rows: 100, col: 15 }
     ];
 
-    classes.forEach(function(c){
-        let classData = readData(c.id);
-        spreadsheets.prepSheet( {
-            name: c.name,
-            templateId: config.template_spreadsheet_id,
-            worksheets: worksheets
-        }).then(function(sheetData) {
-                var members = getRosterData(classData.roster, function(d) {
-                    return d.status == "Active" && memberFilter(d.role);
-                });
+    classes.forEach(function(currentClass){
+        let _this = this;
 
-                var visitors = getRosterData(classData.roster, function(d) {
-                    return d.status == "Active" && !memberFilter(d.role);
-                });
+        try {
+            let classData = readData(currentClass.id);
 
-                var inactive = getInactiveData(classData.roster);
-                var attendance = getAttendanceData(classData.attendance);
-                var emailLists = getEmailLists(classData.roster);
+            if (!classData.roster || !classData.attendance) {
+                return;
+            }
 
-                writeData(sheetData.spreadsheetId, sheetData.worksheets['Roster'], members);
-                writeData(sheetData.spreadsheetId, sheetData.worksheets['Visitors'], visitors);
-                writeData(sheetData.spreadsheetId, sheetData.worksheets['Attendance'], attendance);
-                writeData(sheetData.spreadsheetId, sheetData.worksheets['Email Lists'], emailLists);
-                writeData(sheetData.spreadsheetId, sheetData.worksheets['Inactive'], inactive);
-
-            }).catch(function(err) {
-                console.log(err);
+            //go ahead and prep data before we talk to the Google API
+            var members = getRosterData(classData.roster, function(d) {
+                return d.status == "Active" && memberFilter(d.role);
             });
+
+            var visitors = getRosterData(classData.roster, function(d) {
+                return d.status == "Active" && !memberFilter(d.role);
+            });
+
+            var attendance = getAttendanceData(classData.attendance);
+            var emailLists = getEmailLists(classData.roster);
+
+            spreadsheets.prepSheet( {
+                name: currentClass.name,
+                templateId: config.template_spreadsheet_id,
+                worksheets: worksheets
+            }).then(function(sheetData) {
+                    writeData(currentClass.id, sheetData, 'Roster', members);
+                    writeData(currentClass.id, sheetData, 'Visitors', visitors);
+                    writeData(currentClass.id, sheetData, 'Attendance', attendance);
+                    writeData(currentClass.id, sheetData, 'Email Lists', emailLists);
+
+                }).catch(function(err) {
+                    console.log(err);
+                });
+        } catch(e) {
+            console.log("Error when processing data for classId: " + currentClass.id + " - " + e);
+        }
     });
 }
 
 function readData(classId) {
     let rosterHtml = fs.readFileSync(config.scrape_data_path + '/' + classId + '_roster.html', 'utf8');
     let rosterData = tabletojson.convert(rosterHtml)[0];
+    let result = {
+        roster: null,
+        attendance: null
+    };
 
-    let fieldMappings = _.invert(rosterData[0]);
-    rosterData.shift();
+    if (rosterData && rosterData.length) {
+        let fieldMappings = _.invert(rosterData[0]);
+        rosterData.shift();
 
-    let rosterFormatted = _.map(rosterData, function(d) {
-        return {
-            lastName: d[fieldMappings["last_name"]],
-            firstName: d[fieldMappings["first_name"]],
-            gender: (d[fieldMappings["gender"]] || "") == "0" ? "M" : "F",
-            dob: (d[fieldMappings["person_birthdate"]] && d[fieldMappings["person_birthdate"]].length > 8) ? d[fieldMappings["person_birthdate"]].substring(0,d[fieldMappings["person_birthdate"]].indexOf(' ')) : "",
-            email: d[fieldMappings["person_email"]],
-            cellPhone: d[fieldMappings["mobile_phone"]],
-            homePhone: d[fieldMappings["home_phone"]],
-            address: d[fieldMappings["address"]],
-            cityStateZip: (d[fieldMappings["city"]] || "") + ", " + (d[fieldMappings["state"]] || "") + " " + (d[fieldMappings["postal_code"]] || ""),
-            role: d[fieldMappings["member_role"]],
-            status: d[fieldMappings["date_inactive"]].length > 0 ? "Inactive" : d[fieldMappings["record_status"]],
-            inactiveDate: (d[fieldMappings["date_inactive"]] && d[fieldMappings["date_inactive"]].length >= 8) ? d[fieldMappings["date_inactive"]].substring(0,d[fieldMappings["date_inactive"]].indexOf(' ')) : "",
-        };
-    });
+        result.roster = _.map(rosterData, function(d) {
+            return {
+                lastName: d[fieldMappings["last_name"]],
+                firstName: d[fieldMappings["first_name"]],
+                gender: (d[fieldMappings["gender"]] || "") == "0" ? "M" : "F",
+                dob: (d[fieldMappings["person_birthdate"]] && d[fieldMappings["person_birthdate"]].length > 8) ? d[fieldMappings["person_birthdate"]].substring(0,d[fieldMappings["person_birthdate"]].indexOf(' ')) : "",
+                email: d[fieldMappings["person_email"]],
+                cellPhone: d[fieldMappings["mobile_phone"]],
+                homePhone: d[fieldMappings["home_phone"]],
+                address: d[fieldMappings["address"]],
+                cityStateZip: (d[fieldMappings["city"]] || "") + ", " + (d[fieldMappings["state"]] || "") + " " + (d[fieldMappings["postal_code"]] || ""),
+                role: d[fieldMappings["member_role"]],
+                status: d[fieldMappings["date_inactive"]].length > 0 ? "Inactive" : d[fieldMappings["record_status"]    ]
+            };
+        });
+    }
 
     let attendanceHtml = fs.readFileSync(config.scrape_data_path + '/' + classId + '_attendance.html', 'utf8');
-    let attendanceData = tabletojson.convert(attendanceHtml)[0];
+    result.attendance = tabletojson.convert(attendanceHtml)[0];
 
-    return {
-        roster: rosterFormatted,
-        attendance: attendanceData
-    };
+    return result;
 }
 
-function writeData(spreadsheetId, worksheetId, data) {
+function writeData(classId, sheetData, worksheetName, data) {
+
+    var spreadsheetId = sheetData.spreadsheetId;
+    var worksheetId = sheetData.worksheets[worksheetName];
+
+    if (!data.length || data.length == 1) {
+        return;
+    }
+
+    var padRows = 10;
+    var padColumns = 2;
+
+    //pad extra empty rows and columns
+    for(var i = 1; i <= padRows; i++){
+        var padRow = _.fill(new Array(data[0].length), '');
+        data.push(padRow);
+    }
+
+    data.forEach(function(d, idx){
+        data[idx] = d.concat(_.fill(new Array(padColumns), ''));
+    });
+
     spreadsheetEditor.load({
         debug: true,
         spreadsheetId: spreadsheetId,
@@ -129,53 +161,28 @@ function writeData(spreadsheetId, worksheetId, data) {
             refresh_token: config.refresh_token
         }
     }, function sheetReady(err, spreadsheet) {
-        spreadsheet.add(data);
 
-        spreadsheet.send(function(err) {
-            if (err) throw err;
+        spreadsheet.metadata({
+          title: worksheetName,
+          rowCount: data.length,
+          colCount: data[0].length
+        }, function(err, metadata){
+            if (err) {
+                console.log('Error when setting metadata data for classId:' + classId + ' (' + err + ')');
+            } else {
+                spreadsheet.add(data);
+                spreadsheet.send(function(err) {
+                    if (err) {
+                        console.log('Error when writing data for classId:' + classId + ' (' + err + ')');
+                    }
+                });
+            }
         });
     });
 }
 
 function memberFilter(role){
     return (role.indexOf('Visit') == -1 && !_.contains(['YVNA', 'YMNA'], role));
-}
-
-function getInactiveData(sourceData, filter) {
-    var filtered = _.filter(sourceData, function(d) {
-        return d.status == "Inactive";
-    });
-
-    var formatted = _.map(filtered, function(d){
-        return [
-            d.inactiveDate,
-            d.lastName,
-            d.firstName,
-            d.gender,
-            d.dob,
-            d.email,
-            d.cellPhone,
-            d.homePhone,
-            d.address,
-            d.cityStateZip
-        ];
-    });
-
-    var sorted = _.sortBy(formatted, function(d){
-        var inactiveDateFormatted = '';
-        if (d[0]){
-          var dateGroups = d[0].match(/(\d+)\/(\d+)\/(\d+)/i);
-          inactiveDateFormatted = `${dateGroups[3]}-${dateGroups[1]}-${dateGroups[2]}`;
-        }
-
-        return inactiveDateFormatted;
-    }).reverse();
-
-    //add header
-    var header = ['Inactive As Of', 'Last Name', 'First Name', 'Gender', 'DOB', 'Email', 'Cell Phone', 'Mobile Phone', 'Address', 'City, State Zip'];
-    sorted.unshift(header);
-
-    return sorted;
 }
 
 function getRosterData(sourceData, filter) {
@@ -212,20 +219,33 @@ function getAttendanceData(sourceData) {
 
     //add header with attendance dates
     // i.e.:  Name,	10/25/2015,	10/18/2015
-    for (var i = 9; i <= 20; i++) {
+    var datesStartColumnIndex = 8;
+    var datesEndColumnIndex = _.keys(attendance[0]).length - 1;
+    var datesAvailableCount = (datesEndColumnIndex - datesStartColumnIndex);
+
+    for (var i = datesStartColumnIndex; i <= datesEndColumnIndex; i++) {
         var dirtyDate = attendance[0][i.toString()];
-        attendance[0][i.toString()] = dirtyDate.substring(0, dirtyDate.length - 2);
+        if (dirtyDate && dirtyDate.length) {
+            attendance[0][i.toString()] = dirtyDate.substring(0, dirtyDate.length - 2);
+        }
     }
 
     //ad
     var attenanceFormatted = _.map(attendance, function(d) {
-        return [d["0"], d["20"], d["19"], d["18"], d["17"], d["16"], d["15"], d["14"], d["13"], d["12"], d["11"], d["10"], d["9"]];
+        var attendanceRecord = [d["0"]];
+
+        for(var i = datesEndColumnIndex - 1; i >= datesStartColumnIndex; i--){
+            attendanceRecord.push(d[i]);
+        }
+
+        return attendanceRecord;
     });
 
     return attenanceFormatted;
 }
 
 function getEmailLists(sourceData){
+    var padColumnsCount = 9;
     var active = _.filter(sourceData, function(d){ return d.status == "Active" });
 
     var memberEmails = generateEmailList(active, function(d){ return memberFilter(d.role); });
@@ -233,11 +253,12 @@ function getEmailLists(sourceData){
     var menEmails = generateEmailList(active, function(d){ return d.gender == "M"; });
     var womenEmails = generateEmailList(active, function(d){ return d.gender == "F" });
 
+    var padColumns = _.fill(new Array(padColumnsCount), '');
     return [
-            ['Members', memberEmails],
-            ['Visitors', visitorEmails],
-            ['Men', menEmails],
-            ['Women', womenEmails]
+            ['Members', memberEmails].concat(padColumns),
+            ['Visitors', visitorEmails].concat(padColumns),
+            ['Men', menEmails].concat(padColumns),
+            ['Women', womenEmails].concat(padColumns)
     ];
 }
 
